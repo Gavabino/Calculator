7d06bc170c78fc147e7cb591b08f96afb08c7692
Gavin <gavinatkinson08@gmail.com>
2023-04-16 18:40:36 -0500

    V2

--- src/Basic.java
+++ src/Basic.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class Basic extends Calculator{
-
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        calculator.functionBasic();
+        boolean over = false;
+        while (!over) {
+            System.out.println("Input another? y/n:");
+            String question = scan.next();
+            if (question.equalsIgnoreCase("y")) {
+                calculator.functionBasic();
+            } else {
+                over = true;
+                break;
+            }
+        }
+        scan.close();
+    }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 public class Calculator {
     public Calculator() {
-
     }
-
     public int sum(int value1, int value2) {
         return value1 + value2;
     }
-
     public int difference(int value1, int value2) {
         return value1 - value2;
     }
-
-    public void function() {
+    public int quotient(int value1, int value2) {
+        return value1 / value2;
+    }
+    public int product(int value1, int value2) {
+        return value1 * value2;
+    }
+    public void functionBasic() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Select a function");
         System.out.println("1. Add");
         System.out.println("2. Subtract");
+        System.out.println("3. Divide");
+        System.out.println("3. Multiply");
         int function = scan.nextInt();
         switch (function) {
             case 1 -> {
@@ -33,24 +37,20 @@ public class Calculator {
                 int b = scan.nextInt();
                 System.out.println("Difference: " + this.difference(a, b));
             }
-        }
-    }
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Scanner scan = new Scanner(System.in);
-        calculator.function();
-        boolean over = false;
-        while (!over) {
-            System.out.println("Input another? y/n:");
-            String question = scan.next();
-            if (question.equalsIgnoreCase("y")) {
-                calculator.function();
-            } else {
-                over = true;
-                break;
+            case 3 -> {
+                System.out.print("Enter value one: ");
+                int c = scan.nextInt();
+                System.out.print("Enter value two: ");
+                int d = scan.nextInt();
+                System.out.println("Quotient: " + this.quotient(c, d));
+            }
+            case 4 -> {
+                System.out.print("Enter value one: ");
+                int e = scan.nextInt();
+                System.out.print("Enter value two: ");
+                int f = scan.nextInt();
+                System.out.println("Total: " + this.product(e, f));
             }
         }
-        scan.close();
     }
-
 }
